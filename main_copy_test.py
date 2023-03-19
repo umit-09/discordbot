66ace2b60e627f4759a17d446baf307883631d26
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-03-19 14:16:04 +0100

    Revert "Summary"
    This reverts commit c6fc3d3e9bbd125979a90b0301e525a7eca605e4.

--- main_copy_test.py
+++ main_copy_test.py
@@ -12,7 +12,7 @@ testers = ["852235304965242891","1086242607933440030"]
 data = {"servers":{}}
 cooldown_time = 12 * 60 * 60
 
-with open('secret.secret', 'r') as f:
+with open('.gitignore', 'r') as f:
     TOKENa = f.readline()
     print(TOKENa)
 
