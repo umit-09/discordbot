b2aee045ec952c98719552489d1c2ee46cc7ee18
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-03-27 17:12:10 +0200

    Summary

--- main V3.py
+++ main V3.py
@@ -410,6 +410,32 @@ async def removerole(
             title=None,
             description=f"there is not a saved role named <@&{rolename.id}>",
             colour=random.randint(0, 0xFFFFFF)))
+        
+@bot.command()
+@lightbulb.command("serverinfo", "Get your inventory")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def serverinfo(ctx: lightbulb.SlashContext):
+    # Get the user's ID, username, and the current time
+    server_id = str(ctx.guild_id)
+    data = read_list('data.bin')
+    roles = ""
+    mods = ""
+
+    if server_id in data:
+        for a in data[server_id]["roles"]:
+            roles = roles + f"• <@&{a}>: **{data[server_id]['roles'][a]}**\n"
+        for b in data[server_id]["mods"]:
+            mods = mods + f"• <@{b}>\n"
+
+        await ctx.respond(hikari.Embed(
+            title=None,
+            description=f"**roles:**\n{roles}\n\n**moderators:**\n{mods}",
+            colour=random.randint(0, 0xFFFFFF)))
+    else:
+        await ctx.respond(hikari.Embed(
+            title=None,
+            description=f"this server has nothing",
+            colour=random.randint(0, 0xFFFFFF)))
     
 # Run the bot
 bot.run()
