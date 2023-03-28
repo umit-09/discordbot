db69cbf950347cafc51979939c85eaf242eb4d8d
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-03-28 11:51:51 +0200

    Summary

--- math/multiply.py
+++ math/multiply.py
@@ -1,27 +1,23 @@
 def multiply(x, y):
- 
-    # handle multiplication by 0
-    if x == 0 or y == 0:
-        return 0
- 
+    result = 0
     # determine sign of the result
-    sign = 1
-    if x * y < 0:
-        sign = -1
- 
+    sign = 1 if x >= 0 and y >= 0 or x < 0 and y < 0 else -1
     # convert both numbers to positive
-    x = abs(x)
-    y = abs(y)
- 
-    # compute product and round it to 5 decimal places
-    product = round(x * y, 5)
- 
-    # return the product with the correct sign
-    return sign * product
+    x, y = abs(x), abs(y)
+    # compute product
+    for i in range(y):
+        result += x
+    # round it to 5 decimal places
+    result = round(result, 5)
+    # adjust sign of result if necessary
+    if sign == -1:
+        result = -result
+    # return the product
+    return result
  
 if __name__ == '__main__':
  
-    num1 = 3.56
+    num1 = 3.567
     num2 = 5
  
     print('Product is', multiply(num1, num2))  # Output: Product is 17.8
@@ -36,7 +32,7 @@ if __name__ == '__main__':
  
     print('Product is', multiply(num1, num2))  # Output: Product is -15
 
-    num1 = -3.56
+    num1 = -3.139
     num2 = 5
  
     print('Product is', multiply(num1, num2))  # Output: Product is 15
