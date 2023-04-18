a8c91ebf1eec59f4e8c15d70bd51c76a752c64e1
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-04-18 09:53:08 +0200

    Summary

--- bank-script.js
+++ bank-script.js
@@ -9,14 +9,9 @@ fetch(apiUrl)
     console.log(jsonData);
     if (jsonData[userId]) {
       const userInfo = document.getElementById("user-info");
-
       const balance = jsonData[userId].balance;
       const lastUsed = new Date(jsonData[userId].last_used * 1000);
       const bannerList = jsonData[userId].banner;
-
-      console.log(balance);
-      console.log(lastUsed);
-      console.log(bannerList);
 
       // find the current banner and move it to the front of the list
       const currentBannerIndex = bannerList.indexOf(jsonData[userId].currentbanner);
@@ -30,27 +25,50 @@ fetch(apiUrl)
       for (let i = 0; i < bannerList.length; i++) {
         const bannerName = bannerList[i];
         if (bannerName !== "0") {
-          bannerNames += `<p>${bannerName}</p>`;
+          bannerNames += `
+            <div class="container">
+              <img src="${bannerFolder}${bannerName}.png" alt="${bannerName}" draggable="false">
+              <div class="centered">
+                <h1>${bannerName}</h1>
+              </div>
+            </div>
+          `;
         }
       }
-
-      console.log(bannerNames);
 
       // add the banner names to the user info container
       userInfo.innerHTML = `
         <p>BALANCE: <strong>${balance}</strong></p><br>
         <p>LAST USED:</p>
         <strong><p>${lastUsed.toLocaleString()}</p></strong><br>
-        <p>CURRENT BANNER:</p>
-        <strong><p>${jsonData[userId].currentbanner}</p></strong>
+      `;
+
+      if (jsonData[userId].currentbanner !== "0") {
+        userInfo.innerHTML += `
+          <p>CURRENT BANNER:</p>
+          <div class="container">
+            <img src="${bannerFolder}${jsonData[userId].currentbanner}.png" alt="${jsonData[userId].currentbanner}" draggable="false">
+            <div class="centered">
+              <h1>${jsonData[userId].currentbanner}</h1>
+            </div>
+          </div>
+        `;
+      }
+      else {
+        userInfo.innerHTML += `
+          <p>CURRENT BANNER:</p>
+          <p>This user is not using a banner</p><br>
+        `;
+      }
+
+      userInfo.innerHTML += `
         <p>BANNER(S):</p>
         <strong>${bannerNames}</strong>
       `;
-      if (currentBannerIndex >= 0) {
-        const currentBannerName = bannerList[currentBannerIndex];
-        const currentBannerDiv = document.createElement("p");
-        currentBannerDiv.textContent = currentBannerName;
-        userInfo.insertBefore(currentBannerDiv, userInfo.childNodes[5]);
-      }
+    } else {
+      const userInfo = document.getElementById("user-info");
+      userInfo.innerHTML = `
+        <p>There is no one named <strong>${userId}</strong> registered</p>
+      `;
     }
   });
