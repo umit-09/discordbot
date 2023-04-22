9b0e77c6a8c89503e375dd4a5f0480568ae96f50
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-04-22 13:38:17 +0200

    Update bank-script.js

--- bank-script.js
+++ bank-script.js
@@ -71,9 +71,6 @@ fetch(apiUrl)
       `;
     }
   });
-if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
-  console.log("mobile");
-}
 if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
   document.getElementById("pc").style.display = "none";
   document.getElementById("mobile").style.display = "block";
