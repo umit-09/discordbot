32485b55efd18bc30c9f907c64fa149b097be085
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-05-03 22:24:49 +0200

    Update main_V6.py

--- main_V6.py
+++ main_V6.py
@@ -638,7 +638,7 @@ def hikari_server():
 
     bot.run()
 
-def print_cpu_temp_ram_usage():
+async def print_cpu_temp_ram_usage():
     while True:
         mem = psutil.virtual_memory()
         used = mem.used / (1024 * 1024)  # Convert to MB
@@ -646,7 +646,7 @@ def print_cpu_temp_ram_usage():
         total = mem.total / (1024 * 1024)  # Convert to MB
         
         print(f"RAM usage: {used:.2f} MB\navailable: {available:.2f} MB \ntotal: {total:.2f} MB")
-        time.sleep(60)
+        await asyncio.sleep(60)
 
 thread1 = threading.Thread(target=fastapi_server)
 thread2 = threading.Thread(target=hikari_server)
