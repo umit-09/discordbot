f2df390c35277ed768ae86b34e425f3e727095ad
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-05-03 14:01:15 +0200

    Update main_try_2.py

--- main_try_2.py
+++ main_try_2.py
@@ -1,13 +1,9 @@
-# this code made by umittadelen#4072
-#         main V5
-# ©      copyright 
-#   all rights reserved
-
-import hikari,psutil,lightbulb,random,time,os,json,asyncio,datetime,uvicorn
+import hikari, psutil, lightbulb, random, time, os, json, asyncio, datetime, uvicorn
 from typing import Optional
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
+import concurrent.futures
 
 ###################### define (start) ###########################
 
@@ -46,37 +42,32 @@ except:
 ###################### define (end) ###########################
 
 ###################### start (start) ###########################
+def task_one():
+    app = FastAPI()
+    # Mount the static directory to the app
+    app.mount("/static", StaticFiles(directory="."), name="static")
 
-app = FastAPI()
-# Mount the static directory to the app
-app.mount("/static", StaticFiles(directory="."), name="static")
+    with open('secret.secret', 'r') as f:
+        bot = lightbulb.BotApp(token=f.readline().strip("\n"))
 
-with open('secret.secret', 'r') as f:
-    bot = lightbulb.BotApp(token=f.readline().strip("\n"))
+    @app.get("/bank")
+    async def index() -> JSONResponse:
+        return JSONResponse(read_list("./bank.json"))
 
-# Starts the bot when the server starts
-@app.on_event("startup")
-async def on_startup() -> None:
-    await bot.start()
-    
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(uvicorn.run(app, loop=loop, port=8000))
 
-# Closes the bot when the server closes
-@app.on_event("shutdown")
-async def on_shutdown() -> None:
-    await bot.close()
+def task_two():
+    with open('secret.secret', 'r') as f:
+        bot = lightbulb.BotApp(token=f.readline().strip("\n"))
+    from commands import addmod,addrole,addroleslot,balance,bankinfo,banners,buybanner,buyrole,invite,on_guild_leave,on_started,ping,removemod,removerole,serverinfo,usebanner,vote
+    bot.run()
 
-###################### start (end) ###########################
+async def main():
+    async with asyncio.get_running_loop() as loop:
+        task1 = loop.create_task(task_one())
+        task2 = loop.create_task(task_two())
+        await asyncio.gather(task1, task2)
 
-###################### webserver (start) ###########################
-
-@app.get("/bank")
-async def index() -> JSONResponse:
-    return JSONResponse(read_list("./bank.json"))
-
-###################### webserver (end) ###########################
-
-###################### hikari (start) ###########################
-from commands import addmod,addrole,addroleslot,balance,bankinfo,banners,buybanner,buyrole,invite,on_guild_leave,on_started,ping,removemod,removerole,serverinfo,usebanner,vote
-###################### hikari (end) ###########################
-
-uvicorn.run(app, host="0.0.0.0", port=8000)
+asyncio.run(main())
