603cd7bd1fa34aa6e24e4c0ab34f85b120a75325
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-05-03 19:04:01 +0200

    Update main_try_2.py

--- main_try_2.py
+++ main_try_2.py
@@ -44,7 +44,7 @@ def fastapi_server():
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://umit-09.github.io/discordbot/bank","http://127.0.0.1:5500"],
+        allow_origins=["https://umit-09.github.io/discordbot/bank"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
